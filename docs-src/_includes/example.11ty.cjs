588c69f1c96e533151ba91a3972dee2f6ee4b6e6
bendera <szoftverhiba@gmail.com>
2020-11-03 14:08:35 +0100

    build: filter examples

--- docs-src/_includes/example.11ty.cjs
+++ docs-src/_includes/example.11ty.cjs
@@ -4,8 +4,7 @@ const relative = require('./relative-path.cjs');
 /**
  * This template extends the page template and adds an examples list.
  */
-module.exports = function(data) {
-
+module.exports = function (data) {
   return page({
     ...data,
     content: renderExample(data),
@@ -13,18 +12,39 @@ module.exports = function(data) {
 };
 
 const renderExample = ({name, content, collections, page}) => {
+  const urlParts = page.url.split('/');
+  const baseUrl = `/${urlParts[1]}/${urlParts[2]}/`;
+
+  const filteredExamples =
+    collections.example && collections.example.length > 0
+      ? collections.example.filter(
+          (val) =>
+            val.template.filePathStem.length !== 0 &&
+            val.template.filePathStem.indexOf(baseUrl) === 0
+        )
+      : [];
+
   return `
     <h1>Example: ${name}</h1>
     <section class="examples">
       <nav class="collection">
         <ul>
-          ${collections.example === undefined
+          ${
+            filteredExamples.length === 0
               ? ''
-              : collections.example.map((post) => `
+              : filteredExamples
+                  .map(
+                    (post) => `
                   <li class=${post.url === page.url ? 'selected' : ''}>
-                    <a href="${post.url}">${post.data.description.replace('<', '&lt;')}</a>
+                    <a href="${post.url}">${post.data.description.replace(
+                      '<',
+                      '&lt;'
+                    )}</a>
                   </li>
-                `).join('')}
+                `
+                  )
+                  .join('')
+          }
         </ul>
       </nav>
       <div>
