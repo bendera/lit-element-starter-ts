4e6356251a09a5b271cad64100cce6d9ef3cfb60
bendera <szoftverhiba@gmail.com>
2020-11-03 15:14:44 +0100

    build: fix docs build

--- .eleventy.cjs
+++ .eleventy.cjs
@@ -2,9 +2,12 @@ const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight);
-  eleventyConfig.addPassthroughCopy("dist/bundled.js");
-  eleventyConfig.addPassthroughCopy("docs-src/docs.css");
-  eleventyConfig.addPassthroughCopy("docs-src/.nojekyll");
+  eleventyConfig.addPassthroughCopy({
+    'node_modules/prismjs/themes/prism-okaidia.css': 'prism-okaidia.css',
+  });
+  eleventyConfig.addPassthroughCopy('dist/bundled.js');
+  eleventyConfig.addPassthroughCopy('docs-src/docs.css');
+  eleventyConfig.addPassthroughCopy('docs-src/.nojekyll');
   return {
     dir: {
       input: 'docs-src',
