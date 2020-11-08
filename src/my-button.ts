a9bb9ca652051dfc28150d90d8e9135b531ac489
bendera <szoftverhiba@gmail.com>
2020-11-08 23:35:24 +0100

    style: lint

--- src/my-button.ts
+++ src/my-button.ts
@@ -7,10 +7,16 @@ import {LitElement, html, customElement, property, css} from 'lit-element';
 export class MyButton extends LitElement {
   static styles = css`
     :host {
-      background: linear-gradient(to bottom, #1e5799 0%,#2989d8 50%,#207cca 51%,#7db9e8 100%);
+      background: linear-gradient(
+        to bottom,
+        #1e5799 0%,
+        #2989d8 50%,
+        #207cca 51%,
+        #7db9e8 100%
+      );
       border: 3px solid #70a5e0;
       border-radius: 64px;
-      box-shadow: 0 3px 7px 0 rgba(0, 0, 0, .75);
+      box-shadow: 0 3px 7px 0 rgba(0, 0, 0, 0.75);
       color: #fff;
       cursor: pointer;
       display: inline-block;
@@ -23,17 +29,23 @@ export class MyButton extends LitElement {
 
     :host(:focus) {
       outline: none;
-      border-color:#1e5799;
+      border-color: #1e5799;
     }
 
     :host(:hover) {
-      background: linear-gradient(to bottom, #ebf1f6 0%,#abd3ee 50%,#89c3eb 51%,#d5ebfb 100%);
+      background: linear-gradient(
+        to bottom,
+        #ebf1f6 0%,
+        #abd3ee 50%,
+        #89c3eb 51%,
+        #d5ebfb 100%
+      );
       color: #1e5799;
       text-shadow: none;
     }
 
     :host(:active) {
-      box-shadow: 0 0 7px 0 rgba(0, 0, 0, .75);
+      box-shadow: 0 0 7px 0 rgba(0, 0, 0, 0.75);
       transform: translateY(2px);
     }
   `;
